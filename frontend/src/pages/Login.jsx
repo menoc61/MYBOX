846ffce0e449ccab290fa1499ffa019605048d6e
menoc61 <gillemomeni@gmail.com>
2023-03-18 01:03:13 +0100

    setup cart and wish list functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Login from "../components/Login/Login.jsx";
-
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 const LoginPage = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+// disable redirection if user already login
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>
         <Login />
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Signup from "../components/Signup/Signup";
-
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 const SignupPage = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+// disable redirection if user already login
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>
         <Signup />
